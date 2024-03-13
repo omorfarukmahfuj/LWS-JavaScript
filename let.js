588e0b0e2f72d66b122019cc518511117f01ea57
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-03-14 04:28:10 +0600

    let added

---
+++ let.js
@@ -0,0 +1,12 @@
+// let can't be Redeclared
+// let is block scoped
+
+{
+  var x = 4;
+  let y = 5;
+  console.log(x);
+  console.log(y);
+}
+
+console.log(x);
+console.log(y); // this will give an error
